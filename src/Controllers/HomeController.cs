01a3fb5671cb49cf8f93479d99d2f45412f12cc6
bennaaym <mr.aymen.bennabi@gmail.com>
2021-12-14 15:45:01 +0300

    initial setup

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Book_Store.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
