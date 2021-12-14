b7d18209214a1be9370ff82e2b4ed5b2582efe39
bennaaym <mr.aymen.bennabi@gmail.com>
2021-12-14 19:00:11 +0300

    bennaaym/main: CartItem DomainModel, DTO, Extension added

---
+++ src/Models/ExtensionMethods/CartItemListExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using Book_Store.Models.DomainModels;
+using Book_Store.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Store.Models.ExtensionMethods
+{
+  // add a ToDtoList() method to a List of CartItem objects, to make the code
+  // that converts it to a list of CartItemDTO objects cleaner.
+  public static class CartItemListExtensions
+  {
+    public static List<CartItemDTO> ToDTO(this List<CartItem> list) =>
+      list.Select(ci => new CartItemDTO
+      {
+        BookId = ci.Book.BookId,
+        Quantity = ci.Quantity
+      }).ToList();
+  }
+}
